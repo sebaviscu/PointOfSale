e8d3ab2f30231e6002d111d39f2b62da3a817391
Sebastian Viscusso <sebastian.viscusso@between.tech>
2023-09-13 11:26:01 +0200

    add shop

---
+++ PointOfSale/Controllers/ShopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PointOfSale.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Shop()
+        {
+            return View();
+        }
+    }
+}
