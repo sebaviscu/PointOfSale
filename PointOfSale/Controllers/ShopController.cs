8170b437e55e1f6e7b95871f3e3f3441276ff385
Sebastian Viscusso <sebastian.viscusso@between.tech>
2023-09-15 12:44:48 +0200

    update web

--- PointOfSale/Controllers/ShopController.cs
+++ PointOfSale/Controllers/ShopController.cs
@@ -4,6 +4,7 @@ using PointOfSale.Business.Contracts;
 using PointOfSale.Business.Services;
 using PointOfSale.Model;
 using PointOfSale.Models;
+using System.Globalization;
 using System.Security.Claims;
 
 namespace PointOfSale.Controllers
@@ -26,8 +27,8 @@ namespace PointOfSale.Controllers
         {
             ClaimsPrincipal claimuser = HttpContext.User;
 
-            var shop = new VMShop();
-            shop.Tienda = _mapper.Map<VMTienda>(await _tiendaService.GetTiendaPrincipal());
+            var tienda = _mapper.Map<VMTienda>(await _tiendaService.GetTiendaPrincipal());
+            var shop = new VMShop(tienda);
             shop.Products = _mapper.Map<List<VMProduct>>(await _productService.GetRandomProducts());
             shop.IsLogin = claimuser.Identity.IsAuthenticated;
 
@@ -36,8 +37,14 @@ namespace PointOfSale.Controllers
 
         public async Task<IActionResult> Lista()
         {
-            var vmProductList = _mapper.Map<List<VMProduct>>(await _productService.List());
-            return View("Lista", vmProductList);
+            ClaimsPrincipal claimuser = HttpContext.User;
+            var tienda = _mapper.Map<VMTienda>(await _tiendaService.GetTiendaPrincipal());
+
+            var shop = new VMShop(tienda);
+            shop.IsLogin = claimuser.Identity.IsAuthenticated;
+            shop.Products = _mapper.Map<List<VMProduct>>(await _productService.List());
+
+            return View("Lista", shop);
         }
     }
 }
--- PointOfSale/Models/VMShop.cs
+++ PointOfSale/Models/VMShop.cs
@@ -1,13 +1,51 @@
-﻿using PointOfSale.Model;
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using PointOfSale.Model;
 
 namespace PointOfSale.Models
 {
     public class VMShop
     {
+        public VMShop(VMTienda tienda)
+        {
+            Tienda= tienda;
+
+            Horarios = $"· Lunes: {tienda.Lunes} \n· Martes: {tienda.Martes} \n· Miercoles: {tienda.Miercoles} \n· Jueves: {tienda.Jueves}\n· Viernes: {tienda.Viernes}\n· Sabado: {tienda.Sabado}\n· Domingo: {tienda.Domingo}\n· Feriados: {tienda.Feriado}";
+
+            int day = (int)DateTime.Now.DayOfWeek;
+            switch (day)
+            {
+                case 1:
+                    HorariosToday = "Lunes: " + tienda.Lunes;
+                    break;
+                case 2:
+                    HorariosToday = "Martes: " + tienda.Martes;
+                    break;
+                case 3:
+                    HorariosToday = "Miercoles: " + tienda.Miercoles;
+                    break;
+                case 4:
+                    HorariosToday = "Jueves: " + tienda.Jueves;
+                    break;
+                case 5:
+                    HorariosToday = "Viernes: " + tienda.Viernes;
+                    break;
+                case 6:
+                    HorariosToday = "Sabado: " + tienda.Sabado;
+                    break;
+                case 7:
+                    HorariosToday = "Domingo: " + tienda.Domingo;
+                    break;
+            }
+        }
+
         public List<VMProduct> Products { get; set; }
 
         public VMTienda Tienda { get; set; }
 
         public bool IsLogin { get; set; }
+
+        public string Horarios { get; set; }
+
+        public string HorariosToday { get; set; }
     }
 }
