093e991951dbea591a432ad4960429327571b6fa
Sebastian Viscusso <sebastian.viscusso@hotmail.com>
2025-02-19 09:29:04 -0300

    fix facturar

--- PointOfSale/wwwroot/plugins/externalService.js
+++ PointOfSale/wwwroot/plugins/externalService.js
@@ -119,97 +119,97 @@ async function getLastAuthorizedReceipt(ptoVenta, idTipoComprobante) {
 }
 
 
-//async function getInvoicing(factura) {
-
-//    try {
-//        const response = await fetchWithTimeout(urlPrintService + '/invoice', {
-//            method: 'POST',
-//            headers: {
-//                'Content-Type': 'application/json'
-//            },
-//            body: JSON.stringify(factura),
-//            timeout: 120000 // Timeout de 120 segundos para la solicitud
-//        });
-
-//        const data = await response.json();
-
-//        if (!data.success) {
-//            throw new Error(data.error);
-//        }
-
-//        return data.invoice;
-//    } catch (error) {
-//        if (error.name === 'AbortError') {
-//            console.error('Error: La solicitud de facturar ha excedido el tiempo de espera.');
-//        } else {
-//            console.error('Error a la solicitud de facturar:', error.message);
-//        }
-//        throw error; // Vuelve a lanzar el error para manejarlo en otro lugar si es necesario
-//    }
-//}
-
 async function getInvoicing(factura) {
-    let maxIntentos = 5;
-    let baseTimeout = 2500; // 2 segundos de espera inicial
 
-    for (let intento = 1; intento <= maxIntentos; intento++) {
-        try {
-            const timeoutActual = Math.min(baseTimeout * intento, 120000);
+    try {
+        const response = await fetchWithTimeout(urlPrintService + '/invoice', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(factura),
+            timeout: 120000 // Timeout de 120 segundos para la solicitud
+        });
 
-            await new Promise(resolve => setTimeout(resolve, timeoutActual));
+        const data = await response.json();
 
-            const response = await fetchWithTimeout(urlPrintService + '/invoice', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(factura)
-            });
-
-            const data = await response.json();
-            if (!data.success) {
-                throw new Error(data.error);
-            }
-
-            toastr.options.timeOut = 1500;
-            toastr.info("Facturación realizada");
-
-            return data.invoice;
-        } catch (error) {
-            if (error.name === 'AbortError') {
-                console.error(`Intento ${intento}: La solicitud de facturar ha excedido el tiempo de espera (${baseTimeout * intento}ms).`);
-            } else {
-                let matches = error.message.match(/\b(500|501|502|503)\b/g);
-
-                if (intento != maxIntentos && matches !== null && matches.length > 0) {
-
-                    toastr.options = {
-                        "progressBar": true,
-                        "timeOut": (baseTimeout * intento) + 1500
-                    };
-
-                    toastr.warning(`Intento ${intento}, próximo en ${(baseTimeout * intento) / 1000}s`, "Facturación");
-
-                    //console.error(`Intento ${intento}, próximo en ${(baseTimeout * intento) / 1000}s: Error en la solicitud de facturar:`, error.message);
-                }
-
-                if (intento == 2) {
-                    removeLoading();
-                }
-
-                if (intento === maxIntentos || matches.length === 0) {
-                    showLoading();
-                    throw error; // Si ya intentó 5 veces, lanza el error final
-                }
-            }
-        } finally {
-            toastr.options = {
-                "progressBar": true,
-                "timeOut": 5000
-            };
+        if (!data.success) {
+            throw new Error(data.error);
         }
+
+        return data.invoice;
+    } catch (error) {
+        if (error.name === 'AbortError') {
+            console.error('Error: La solicitud de facturar ha excedido el tiempo de espera.');
+        } else {
+            console.error('Error a la solicitud de facturar:', error.message);
+        }
+        throw error; // Vuelve a lanzar el error para manejarlo en otro lugar si es necesario
     }
 }
+
+//async function getInvoicing(factura) {
+//    let maxIntentos = 5;
+//    let baseTimeout = 2500; // 2 segundos de espera inicial
+
+//    for (let intento = 1; intento <= maxIntentos; intento++) {
+//        try {
+//            const timeoutActual = Math.min(baseTimeout * intento, 120000);
+
+//            await new Promise(resolve => setTimeout(resolve, timeoutActual));
+
+//            const response = await fetchWithTimeout(urlPrintService + '/invoice', {
+//                method: 'POST',
+//                headers: {
+//                    'Content-Type': 'application/json'
+//                },
+//                body: JSON.stringify(factura)
+//            });
+
+//            const data = await response.json();
+//            if (!data.success) {
+//                throw new Error(data.error);
+//            }
+
+//            toastr.options.timeOut = 1500;
+//            toastr.info("Facturación realizada");
+
+//            return data.invoice;
+//        } catch (error) {
+//            if (error.name === 'AbortError') {
+//                console.error(`Intento ${intento}: La solicitud de facturar ha excedido el tiempo de espera (${baseTimeout * intento}ms).`);
+//            } else {
+//                let matches = error.message.match(/\b(500|501|502|503)\b/g);
+
+//                if (intento != maxIntentos && matches !== null && matches.length > 0) {
+
+//                    toastr.options = {
+//                        "progressBar": true,
+//                        "timeOut": (baseTimeout * intento) + 1500
+//                    };
+
+//                    toastr.warning(`Intento ${intento}, próximo en ${(baseTimeout * intento) / 1000}s`, "Facturación");
+
+//                    //console.error(`Intento ${intento}, próximo en ${(baseTimeout * intento) / 1000}s: Error en la solicitud de facturar:`, error.message);
+//                }
+
+//                if (intento == 2) {
+//                    removeLoading();
+//                }
+
+//                if (intento === maxIntentos || matches.length === 0) {
+//                    showLoading();
+//                    throw error; // Si ya intentó 5 veces, lanza el error final
+//                }
+//            }
+//        } finally {
+//            toastr.options = {
+//                "progressBar": true,
+//                "timeOut": 5000
+//            };
+//        }
+//    }
+//}
 
 
 
@@ -253,36 +253,37 @@ async function InvoiceSale(imprimirTicket, saleResult) {
             try {
                 let nroComprobante = await getLastAuthorizedReceipt(f.cabecera.puntoVenta, f.cabecera.tipoComprobante.id);
 
+                nroComprobante++;
                 for (let d of f.detalle) {
-                    nroComprobante++;
                     d.nroComprobanteDesde = nroComprobante;
                     d.nroComprobanteHasta = nroComprobante;
-
-                    await new Promise(resolve => setTimeout(resolve, 500)); // Espera 0.5 segundos
-
-                    try {
-                        let i = await getInvoicing(f);
-
-                        let saveInvoice = {
-                            facturacion: i,
-                            idFacturaEmitida: parseInt(f.idFacturaEmitida)
-                        };
-
-                        let response = await fetch("/Facturacion/SaveInvoice", {
-                            method: "POST",
-                            headers: { 'Content-Type': 'application/json;charset=utf-8' },
-                            body: JSON.stringify(saveInvoice)
-                        });
-
-                        let responseInvoice = await response.json();
-
-                        removeLoading();
-                        printTicketSale(imprimirTicket, saleResult, responseInvoice.object);
-                    } catch (error) {
-                        toastr.warning("La venta fue registrada correctamente, pero no se ha podido facturar.", "Error al Facturar");
-                        saveNotificationInvoiceError(saleResult.saleNumber, error, parseInt(f.idFacturaEmitida));
-                    }
                 }
+
+                await new Promise(resolve => setTimeout(resolve, 500)); // Espera 0.5 segundos
+
+                try {
+                    let i = await getInvoicing(f);
+
+                    let saveInvoice = {
+                        facturacion: i,
+                        idFacturaEmitida: parseInt(f.idFacturaEmitida)
+                    };
+
+                    let response = await fetch("/Facturacion/SaveInvoice", {
+                        method: "POST",
+                        headers: { 'Content-Type': 'application/json;charset=utf-8' },
+                        body: JSON.stringify(saveInvoice)
+                    });
+
+                    let responseInvoice = await response.json();
+
+                    removeLoading();
+                    printTicketSale(imprimirTicket, saleResult, responseInvoice.object);
+                } catch (error) {
+                    toastr.warning("La venta fue registrada correctamente, pero no se ha podido facturar.", "Error al Facturar");
+                    saveNotificationInvoiceError(saleResult.saleNumber, error, parseInt(f.idFacturaEmitida));
+                }
+
             } catch (error) {
                 toastr.warning("La venta fue registrada correctamente, pero no se ha podido facturar.", "Error al Facturar");
                 saveNotificationInvoiceError(saleResult.saleNumber, error, null);
